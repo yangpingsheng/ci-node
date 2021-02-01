70eae66f11134faf15ded4b6f2729204e2d68d78
pish7 <949428190@qq.com>
2021-02-01 11:57:41 +0800

    配置 ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
+const env = process.env.NODE_ENV
+
 module.exports = {
+  root: true,
   env: {
     node: true,
+    jest: true,
   },
   parser: '@typescript-eslint/parser',
   extends: [
@@ -11,11 +15,14 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
+    'no-console': env === 'production' ? 'error' : 'off',
+    'no-debugger': env === 'production' ? 'error' : 'off',
+    'no-unused-vars': ['error', { args: 'none' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/member-delimiter-style': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-extra-semi': 'warn',
-    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
   },
 }
